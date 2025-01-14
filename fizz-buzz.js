fb289f18c8ee7fecc22e680cbaa59a3a865319e5
Mark Babirad <mbabirad@brainstation.io>
2025-01-14 14:11:46 -0800

    initialize project

---
+++ fizz-buzz.js
@@ -0,0 +1,16 @@
+function fizzBuzz(number) {
+  if (number % 15 === 0) {
+    return "FizzBuzz";
+  }
+
+  if (number % 3 === 0) {
+    return "Fizz";
+  }
+
+  if (number % 5 === 0) {
+    return "Buzz";
+  }
+
+  return number;
+}
+export default fizzBuzz;
---
+++ tests/fizz-buzz.test.js
@@ -0,0 +1,27 @@
+import fizzBuzz from "./fizz-buzz";
+import { test, expect, it, describe } from "vitest";
+
+describe("fizzBuzz function", () => {
+  it("returns 'FizzBuzz' for multiples of 3 and 5", () => {
+    expect(fizzBuzz(15)).toBe("FizzBuzz");
+    expect(fizzBuzz(30)).toBe("FizzBuzz");
+    expect(fizzBuzz(45)).toBe("FizzBuzz");
+  });
+
+  it('returns "Fizz" for multiples of 3', () => {
+    expect(fizzBuzz(3)).toBe("Fizz");
+    expect(fizzBuzz(33)).toBe("Fizz");
+    expect(fizzBuzz(9)).toBe("Fizz");
+  });
+
+  it('returns "Buzz" for multiples of 5', () => {
+    expect(fizzBuzz(5)).toBe("Buzz");
+    expect(fizzBuzz(20)).toBe("Buzz");
+    expect(fizzBuzz(50)).toBe("Buzz");
+  });
+
+  it("returns the given number for multiples of neither 3 nor 5", () => {
+    expect(fizzBuzz(1)).toBe(1);
+    expect(fizzBuzz(22)).toBe(22);
+  });
+});
